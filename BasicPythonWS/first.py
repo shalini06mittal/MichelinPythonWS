bf8d351a99062a89ae20d1044e50ec5520b50b4f
Shalini Mittal <shalini06.mittal@gmail.com>
2023-12-11 12:38:09 +0530

    first commit

---
+++ BasicPythonWS/first.py
@@ -0,0 +1,21 @@
+print('hello')
+# variables
+x = 100
+y = 20
+sum = x+y
+print(x+y)
+print(sum)
+# 10+20=30
+print(x,'+',y,'=',sum)
+# str()-> int to str
+print(str(x)+' + '+str(y)+' = '+str(sum))
+# data types
+print(type(2))
+print(type(2.3))
+print(type('2'))
+print(type('a'))
+print(type("2"))
+print(type("shalini"))
+print(type(True)) # False
+print(type('True'))
+print(type(2+3j))
---
+++ BasicPythonWS/operators.py
@@ -0,0 +1,50 @@
+# to take input 
+# variable are always on the LHS of =
+# RHS of = is the expression whose result you want to store in the variable
+# \n -> escape sequence
+# name = input('enter your name\n')
+# print('Welcome', name)
+# int() -> str to int
+# single line comment
+'''
+multi line comments
+aksjdsla
+asd
+as
+dsa
+'''
+"""
+asasdasd
+asdasd
+"""
+# n1 = int(input('enter a number'))
+# n2 = int(input('enter a number'))
+# print(n1+n2)
+
+# operators
+'''
+Arithmetic operators
++ - * / % ** //(floor division)
+'''
+'''
+relational / comparison
+< > <= >= == != 
+work on some data and return a boolean result
+'''
+# print(2>3)
+# print('shalini' == 'shalini')
+'''
+logical operators => applied on result of relational expressions
+work on boolean values and return boolean result
+and or
+25%3==0 and 25%5==0
+F and T
+login screen -> username and password
+options register -> g, l, f, form
+'''
+print(25%3 == 0 and 25%5 == 0)
+print(25%3==0 or 25%5==0)
+#indentation => spaces
+print(25%3)
+
+

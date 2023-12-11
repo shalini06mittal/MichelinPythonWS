3369a345615739964cd6489968198a2f2bca7408
Shalini Mittal <shalini06.mittal@gmail.com>
2023-12-11 13:30:30 +0530

    day 1 demos

--- BasicPythonWS/loop.py
+++ BasicPythonWS/loop.py
@@ -59,10 +59,20 @@ while <conditional expression>:
 # print sqaures of random numbers taken as input from the user  ,
 # until user quits => 0
 
-stop = -1
-while stop != 0:
-    no = int(input('enter a no'))
+# stop = -1
+# while stop != 0:
+#     no = int(input('enter a no'))
+#     print(no * no)
+#     stop = int(input('enter 0 to stop or any value to continue'))
+#     if stop == 0:
+#         print('Thanks')
+
+
+while True:
+    no = int(input('enter a no to calculate square or 0 to stop'))
+    if no == 0:
+         print('Thanks')
+         break # jump statement that breaks the exwcution of the loop
     print(no * no)
-    stop = int(input('enter 0 to stop or any value to continue'))
-    if stop == 0:
-        print('Thanks')
+  
+       

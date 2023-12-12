8710fe6f62a857e9b3636ce854dcddf4c66ac3db
Shalini Mittal <shalini06.mittal@gmail.com>
2023-12-12 11:03:06 +0530

    day 1 demos

---
+++ BasicPythonWS/iddemo.py
@@ -0,0 +1,9 @@
+x= 'shalini'
+print(id(x))
+x=10
+print(id(x));
+if x==10:
+    pass
+# if=10
+print(type(x))
+# print(int)
---
+++ BasicPythonWS/printbasics.py
@@ -0,0 +1,3 @@
+print('hey', end=' ') # print the output and take the cursor to next line
+print('welcome')
+print(1,'is a lucky number', 10, sep='#')

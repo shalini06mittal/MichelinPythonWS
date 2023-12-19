6c1d1417703398bb987757265aa93e23f272dced
Shalini Mittal <shalini06.mittal@gmail.com>
2023-12-19 12:38:55 +0530

    functions lambda

--- BasicPythonWS/functions/lambdas.py
+++ BasicPythonWS/functions/lambdas.py
@@ -14,14 +14,37 @@ sq = lambda no : no*no
 
 # print(sq(10))
 
-names = ['Shalini Mittal','Ajay Gupta','Manas Sharma','Joe Austin']
+names = ['Shalini Mittal','Ajay Gupta','Manas Sharma','Joe Austin','Ria Sana']
 
-print(names)
+# bubble sort
+for i in range(len(names)-1):
+    for j in range(len(names)-1):
+        if len(names[j]) > len(names[j+1]):
+            temp = names[j]
+            names[j] = names[j+1]
+            names[j+1] = temp
+        
+
+# print(names)
 # def splitnames(name):
 #     print(name)
 #     return name.split()[1]
 
+def length(name):
+    c = 0
+    for v in name: 
+        c+=1
+    return c
+
+def outer(x):
+    def inner(y):
+        return x+y
+    
+x = lambda x: lambda y : x+y
+
 names.sort(key=lambda name:name.split()[1] )
+print(names)
+names.sort(key=length )
 print(names)
 
 '''
@@ -29,3 +52,14 @@ create a list of 2-digit numbers and sort the list on the basis of last digit
 23 89 45 76 32
 32 23 45 76 89
 '''
+
+# def lastDigit(no):
+#     return no%10
+
+list_1 = [12,34,56,16,45,78,95,56]
+
+list_1.sort(key = lambda no: no%10)
+
+# print(list_1)
+
+

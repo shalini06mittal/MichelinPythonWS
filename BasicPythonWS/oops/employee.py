386cb980f44c643dc075c4975dab64f8acebde04
Shalini Mittal <shalini06.mittal@gmail.com>
2023-12-21 13:27:01 +0530

    python class demo

--- BasicPythonWS/oops/employee.py
+++ BasicPythonWS/oops/employee.py
@@ -6,8 +6,13 @@ class Employee:
         self.email = email
         self.phone = phone
         self.salary = salary
+
     def setSalary(self, salary):
+        def f1():
+            print('f1 called', salary)
         self.salary = salary
+        return f1
+    
     def __str__(self) -> str:
         return self.name+' '+self.email+' '+ self.phone+' '+str(self.salary)
 
@@ -16,6 +21,9 @@ print(e1.name,e1.phone,e1.email)
 e2 = Employee('ajay','ajay@gmail.com','123123')
 print(e2.name,e2.phone,e2.email)
 print(e1) # string representation of employee e1
-e1.setSalary(121212)
+func = e1.setSalary(109080)
 print(e1)
+func()
+e2.setSalary(68789)
+print(e2)
         
